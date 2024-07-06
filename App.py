7e0951215be146c13d8b564e75ff07aa9a70ffa3
ffgtv3 <167542747+ffgtv3@users.noreply.github.com>
2024-07-06 18:37:57 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,28 @@
-import flask
+# Импортировать библиотеку Flask
+from flask import Flask, request, redirect, url_for
 
-app = flask.Flask(__name__)
+# Создать экземпляр приложения Flask
+app = Flask(__name__)
 
+# Определить маршрут для корневого URL-адреса ('/')
 @app.route('/')
-def redirect_to_discord():
-    return flask.redirect('https://discord.com/invite/mEwDqSV4Yk')
+def index():
+    # Проверить, есть ли в запросе параметр 'url'
+    url = request.args.get('url')
 
+    # Если параметра 'url' нет, перенаправить на домашнюю страницу
+    if not url:
+        return redirect(url_for('home'))
+
+    # В противном случае перенаправить на указанный URL-адрес
+    return redirect(url)
+
+# Определить маршрут для домашней страницы ('/home')
+@app.route('/home')
+def home():
+    # Возвращает сообщение с домашней страницы
+    return '<h1>Добро пожаловать на домашнюю страницу!</h1>'
+
+# Запустить приложение
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
